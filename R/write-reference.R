51f069780b70206f1b0318af1c29f7be48d5f4f7
Edgar Ruiz <edgararuiz@gmail.com>
2024-10-11 11:33:20 -0500

    Adds template and example args

--- R/write-reference.R
+++ R/write-reference.R
@@ -2,23 +2,29 @@
 write_reference_index <- function(
     pkg = ".",
     folder = "reference",
-    file = "index.qmd") {
+    file = "index.qmd",
+    template = system.file("templates/_index.qmd", package = "pkgsite")) {
   if (is.character(pkg)) pkg <- pkgdown::as_pkgdown(pkg)
   try(dir_create(folder))
   ref <- path(folder, file)
   try(file_delete(ref))
-  writeLines(reference_index(pkg), ref)
+  writeLines(reference_index(pkg, template), ref)
   cli_inform(col_green(ref))
 }
 
 #' @export
-write_reference <- function(pkg = ".", folder = "reference") {
+write_reference <- function(
+    pkg = ".",
+    folder = "reference",
+    examples = TRUE,
+    not_run_examples = FALSE,
+    template = system.file("templates/_reference.qmd", package = "pkgsite")) {
   if (is.character(pkg)) pkg <- pkgdown::as_pkgdown(pkg)
   walk(
     pkg$topics$file_in,
     \(x) {
       ref <- paste0(folder, "/", path_ext_remove(x), ".qmd")
-      qmd <- rd_to_qmd(x, pkg)
+      qmd <- rd_to_qmd(x, pkg, examples, not_run_examples, template)
       try(file_delete(ref))
       writeLines(qmd, ref)
       cli_inform(col_green(ref))
