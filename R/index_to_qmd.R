3cf3a8b7b255d5dc4dd9046d28f7895de602de5f
Edgar Ruiz <edgararuiz@gmail.com>
2024-11-29 08:04:46 -0600

    Accomodates improvements on _quarto.yml pkgsite structure

--- R/index_to_qmd.R
+++ R/index_to_qmd.R
@@ -19,35 +19,31 @@ index_to_qmd <- function(
 }
 
 reference_index_convert <- function(pkg, index = NULL) {
-  
   rd_names <- path_file(dir_ls(path(pkg, "man"), glob = "*.Rd"))
+  qmd_names <- path(path_ext_remove(rd_names), ext = "qmd")
   rd_list <- map(rd_names, rd_to_list, pkg)
-  rd_list <- set_names(rd_list, rd_names)
-  
-  ref_lines <- imap(rd_list, \(x, y) {
-    qmd_name <- path(path_ext_remove(y), ext = "qmd")
+  rd_list <- set_names(rd_list, qmd_names)
+
+  all_refs <- imap(rd_list, \(x, y) {
     alias <- as.character(x[names(x) == "alias"])
-    alias_links <- paste0("[", alias, "()](", qmd_name,")")
+    alias_links <- paste0("[", alias, "()](", y, ")")
     c(
       paste0(alias_links, collapse = " "),
-      "", "", 
+      "", "",
       paste0("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;", x$title),
       ""
     )
   })
-  ref_names <- path_ext_remove(names(ref_lines))
-  ref_lines <- set_names(ref_lines, ref_names)
-  
-  if(!is.null(index)) {
-    sections <- index[["contents"]][["sections"]]
-    if(!is.null(sections)) {
-      ref_lines <- map(contents, \(x) {
-        refs <- map(x$contents, \(y) ref_lines[names(ref_lines) == y])
-        c(list(list(title = paste0("##", x$title), "")), refs)
-      })
-      ref_lines <- list_flatten(ref_lines)
-      ref_lines <- reduce(ref_lines, c)
-    }
+  if (!is.null(index)) {
+    contents <- index[["contents"]]
+    ref_lines <- map(contents, \(x) {
+      if (!is.null(x[["section"]])) {
+        refs <- map(x[["contents"]], \(y) all_refs[names(all_refs) == y])
+        out <- c(list(list(title = paste("##", x$section), "")), refs)
+      }
+    })
+    ref_lines <- list_flatten(ref_lines)
+    ref_lines <- reduce(ref_lines, c)
   }
   ref_lines <- reduce(ref_lines, c)
   list("reference" = ref_lines)
